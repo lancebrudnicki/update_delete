475995dd03f4efa1a27870dc8c2b2f6b65cb8b1b
lancebrudnicki <lancebrudnicki@gmail.com>
2021-08-11 19:17:31 -0600

    show

--- main/java/com/lance/mvc/controllers/BooksController.java
+++ main/java/com/lance/mvc/controllers/BooksController.java
@@ -1,5 +1,7 @@
 package com.lance.mvc.controllers;
 
+
+
 import java.util.List;
 
 import javax.validation.Valid;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -40,5 +43,11 @@ public class BooksController {
             bookService.createBook(book);
             return "redirect:/books";
         }
+    }
+    @RequestMapping(value="/books/{id}")
+    public String show(@PathVariable("id") Long id, Model model) {
+    	Book book = bookService.findBook(id);
+    	model.addAttribute("book", book);
+    	return "/books/show.jsp";
     }
 }
